ad168838b83d7b4cfa1644712a7611fd29af074d
skyneting <zhoucheng2007@gmail.com>
2014-12-09 18:58:27 +0800

    提交代码

--- src/service/HelloApp.java
+++ src/service/HelloApp.java
@@ -5,6 +5,8 @@ public class HelloApp {
 	  * 对外公开的服务方法
 	  */
 	 public String sayHello(String name) {
+		 System.out.println("接收到的参数为"+name);
+		 System.out.println("返回的参数为"+name);
 	  return "Hello," + name;
 	 }
 	 
@@ -12,6 +14,7 @@ public class HelloApp {
 	  * 对外公开的服务方法
 	  */
 	 public String saySorry(String name,String word) {
+		 System.out.println("接收到的参数为"+name+"   "+word);
 	  return "sorry," + name+", beause of" +word;
 	 }
 }
